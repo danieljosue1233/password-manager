12b7a8c56c77c517cb5a18d4dcdd9aef5691569c
danieljosue1233 <danieljosuemartinezjarquin3@gmail.com>
2024-11-20 21:36:30 -0600

    feat: private routes have been added

---
+++ password/app/(routes)/(home)/layout.tsx
@@ -0,0 +1,31 @@
+import Logo from "@components/Shared/Logo";
+import Sidebar from "@components/Shared/Sidebar/Sidebar";
+import { SidebarMobile } from "@components/Shared/SidebarMobile";
+
+
+const LayoutRoutes = ({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) => {
+    return (
+        <main className="h-full">
+            <div className="flex justify-between lg:hidden px-6 py-3 items-center bg-blue-800">
+                <div className="py-1 text-white">
+                    <Logo />
+                </div>
+                <SidebarMobile />
+            </div>
+            <div className="flex h-full">
+                <div className="max-w-lg hidden lg:flex h-full fixed w-72
+                flex-col bg-blue-800 px-4 text-white">
+                    <Sidebar />
+                </div>
+                <div className="w-full lg:pl-72">
+                    <div className="p-6">{children}</div>
+                </div>
+            </div>
+        </main>
+    )
+}
+export default LayoutRoutes
---
+++ password/app/(routes)/(home)/page.tsx
@@ -0,0 +1,10 @@
+import HeaderMain from "../../../components/HeaderMain/HeaderMain";
+
+const Home = () => {
+  return (
+    <div>
+      <HeaderMain />
+    </div>
+  );
+}
+export default Home
