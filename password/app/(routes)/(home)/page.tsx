e923b707f679059b29dc20c7b3c8a3dc25667a89
danieljosue1233 <danieljosuemartinezjarquin3@gmail.com>
2024-11-22 20:24:14 -0600

    feat: tableData was added to pass the created elements to the table and the userId

--- password/app/(routes)/(home)/page.tsx
+++ password/app/(routes)/(home)/page.tsx
@@ -1,9 +1,37 @@
+import { getServerSession } from "next-auth";
 import HeaderMain from "../../../components/HeaderMain/HeaderMain";
+import { redirect } from "next/navigation";
+import { db } from "@lib/db";
+import TableData from "@components/TableData/TableData";
 
-const Home = () => {
+const Home = async () => {
+  const session = await getServerSession()
+
+  if (!session || !session.user?.email) {
+    return redirect("/");
+  }
+
+  const user = await db.user.findUnique({
+    where: {
+      email: session?.user.email
+    },
+    include: {
+      elements: {
+        orderBy: {
+          createAt: "desc"
+        }
+      }
+    }
+  })
+
+  if (!user || !user.elements) {
+    return redirect("/")
+  }
+
   return (
     <div>
-      <HeaderMain />
+      <HeaderMain userId={user.id} />
+      <TableData elements={user.elements} />
     </div>
   );
 }
